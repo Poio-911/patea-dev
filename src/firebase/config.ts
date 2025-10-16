056c89ea8591ec630c3c5be3f4030d5ade09f610
Santiago Lopez <Santiago.lopez.toma@gmail.com>
2025-10-16 20:13:10 +0000

    que paso? y

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -6,10 +6,10 @@
 import { FirebaseOptions } from 'firebase/app';
 
 export const firebaseConfig: FirebaseOptions = {
-  apiKey: "AIzaSyDP4vP1q5-XXXXXXXX-XXXXXXXX-XXXXXXX",
-  authDomain: "mil-disculpis.firebaseapp.com",
-  projectId: "mil-disculpis",
-  storageBucket: "mil-disculpis.appspot.com",
-  messagingSenderId: "1024088317155",
-  appId: "1:1024088317155:web:a6217c376f906d489b4b01",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
