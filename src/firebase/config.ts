690b2dddb819d17ae07676c12d11dbe0f41e25ac
Santiago Lopez <Santiago.lopez.toma@gmail.com>
2025-10-16 20:05:08 +0000

    No. tira el mismo error

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -6,10 +6,10 @@
 import { FirebaseOptions } from 'firebase/app';
 
 export const firebaseConfig: FirebaseOptions = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "YOUR_API_KEY",
+  authDomain: "futbol-app-pwa-25266201-1bba5.firebaseapp.com",
+  projectId: "futbol-app-pwa-25266201-1bba5",
+  storageBucket: "futbol-app-pwa-25266201-1bba5.appspot.com",
+  messagingSenderId: "843616956629",
+  appId: "1:843616956629:web:9c22b109632360f0892095",
 };
