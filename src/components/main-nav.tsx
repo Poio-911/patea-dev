c1c34bde538e27c1e919e91e7674b8f9d7694460
Santiago Lopez <Santiago.lopez.toma@gmail.com>
2025-10-17 14:54:15 +0000

    en el menú de abajo no veo el icono que te pase, debería ir ahi en el me

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -18,7 +18,7 @@ import {
 } from '@/components/ui/sidebar';
 import { Button } from '@/components/ui/button';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-import { LayoutDashboard, Calendar, LogOut, Settings, Goal, Users2, ShieldQuestion, User, Globe } from 'lucide-react';
+import { LayoutDashboard, LogOut, Settings, Goal, Users2, ShieldQuestion, User, Globe } from 'lucide-react';
 import { cn } from '@/lib/utils';
 import { Separator } from './ui/separator';
 import { useUser, useAuth, useDoc, useFirestore } from '@/firebase';
@@ -36,12 +36,13 @@ import type { Player } from '@/lib/types';
 import { doc } from 'firebase/firestore';
 import { Badge } from '@/components/ui/badge';
 import { SoccerPlayerIcon } from './icons/soccer-player-icon';
+import { MatchIcon } from './icons/match-icon';
 
 
 const navItems = [
   { href: '/dashboard', label: 'Panel', icon: LayoutDashboard },
   { href: '/players', label: 'Jugadores', icon: SoccerPlayerIcon },
-  { href: '/matches', label: 'Partidos', icon: Calendar },
+  { href: '/matches', label: 'Partidos', icon: MatchIcon },
   { href: '/evaluations', label: 'Evaluaciones', icon: ShieldQuestion },
   { href: '/find-match', label: 'Buscar', icon: Globe },
 ];
