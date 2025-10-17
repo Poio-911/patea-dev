be1e8c0d59c5eb4716d2c036e584efd40cc584b1
Santiago Lopez <Santiago.lopez.toma@gmail.com>
2025-10-17 02:50:34 +0000

    Ahora un cambio visual mas, las player card, hoy en mobile se muestran d

--- src/app/players/page.tsx
+++ src/app/players/page.tsx
@@ -88,7 +88,7 @@ export default function PlayersPage() {
         </Alert>
       )}
 
-      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+      <div className="grid grid-cols-2 gap-6 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
         {players?.map((player) => (
           <PlayerCard key={player.id} player={player} />
         ))}
