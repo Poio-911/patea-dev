94173a389822a37e6ea754da6555bc288cebfd18
Santiago Lopez <Santiago.lopez.toma@gmail.com>
2025-10-18 05:38:10 +0000

    que otra forma de cargar imagenes hay si no podemos resolver esto?

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,19 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          },
+        ],
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
