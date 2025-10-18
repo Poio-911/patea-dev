509f90496e5a26357d3f792b2bd74c1c2c893b07
Santiago Lopez <Santiago.lopez.toma@gmail.com>
2025-10-18 02:23:35 +0000

    starting build "83e2c4b9-4216-499c-bcee-2d53ef90d5e7" FETCHSOURCE BUILD

--- src/firebase/error-emitter.ts
+++ src/firebase/error-emitter.ts
@@ -1,3 +1,4 @@
+
 import { EventEmitter } from 'eventemitter3';
 import { FirestorePermissionError } from './errors';
 
@@ -5,14 +6,4 @@ type Events = {
   'permission-error': (error: FirestorePermissionError) => void;
 };
 
-class TypedEventEmitter extends EventEmitter {
-  emit<E extends keyof Events>(event: E, ...args: Parameters<Events[E]>) {
-    return super.emit(event, ...args);
-  }
-
-  on<E extends keyof Events>(event: E, listener: Events[E]) {
-    return super.on(event, listener);
-  }
-}
-
-export const errorEmitter = new TypedEventEmitter();
+export const errorEmitter = new EventEmitter<Events>();
