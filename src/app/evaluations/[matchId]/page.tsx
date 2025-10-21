1315119947feb611a67d8f4596a62312fe296fb0
Santiago Lopez <Santiago.lopez.toma@gmail.com>
2025-10-21 15:14:23 +0000

    este commit elimino todos los archvios
    Y puede ser el problema

--- src/app/evaluations/[matchId]/page.tsx
+++ src/app/evaluations/[matchId]/page.tsx
@@ -2,9 +2,10 @@
 'use client';
 
 import PerformEvaluationView from '@/components/perform-evaluation-view';
+import { useParams } from 'next/navigation';
 
-export default function PerformEvaluationPage({ params }: { params: { matchId: string } }) {
-  const { matchId } = params;
+export default function PerformEvaluationPage() {
+  const { matchId } = useParams();
 
   if (!matchId || typeof matchId !== 'string') {
     return <div>ID de partido no válido.</div>;
