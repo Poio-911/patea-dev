f010026f3486fb8b24c0653ea3f369cb99ab1933
Santiago Lopez <Santiago.lopez.toma@gmail.com>
2025-10-21 04:15:01 +0000

    Dale

--- src/components/perform-evaluation-view.tsx
+++ src/components/perform-evaluation-view.tsx
@@ -1,4 +1,3 @@
-
 'use client'
 
 import { useState, useEffect, useMemo, useCallback } from 'react'
@@ -7,7 +6,16 @@ import Link from 'next/link'
 import { useForm, useFieldArray, Controller } from 'react-hook-form'
 import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { doc, writeBatch, collection, query, where, addDoc, getDocs, orderBy } from 'firebase/firestore'
+import { 
+  doc, 
+  writeBatch, 
+  collection, 
+  query, 
+  where, 
+  addDoc, 
+  getDocs, 
+  orderBy 
+} from 'firebase/firestore'
 import { Loader2, Save, ShieldCheck, Goal, Plus, Minus, FileClock } from 'lucide-react'
 
 import { useFirestore, useUser, useCollection } from '@/firebase'
