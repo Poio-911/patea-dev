f627bf3d3ea26ecdb4c2d64d3311dcf2fc0438eb
Santiago Lopez <Santiago.lopez.toma@gmail.com>
2025-10-21 14:44:34 +0000

    Starting preview... Waiting for your web server to start serving
    > next

--- next.config.js
+++ next.config.js
@@ -1,50 +1,9 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Referrer-Policy',
-            value: 'strict-origin-when-cross-origin',
-          },
-        ],
-      },
-    ];
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'firebasestorage.googleapis.com',
-        port: '',
-        pathname: '/**',
-      }
-    ],
-  },
-  experimental: {},
-  allowedDevOrigins: ["https://*.cloudworkstations.dev"],
+    experimental: {
+        allowedDevOrigins: ["https://*.cloudworkstations.dev"],
+    },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
