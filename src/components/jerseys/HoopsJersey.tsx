ecd5e5702bed4bf4ef86ac4c0ca3d2e3cb0b09cb
Santiago Lopez <Santiago.lopez.toma@gmail.com>
2025-10-22 20:11:51 +0000

    eso no es una camiseta, sigue mal es una puta pelota

--- src/components/jerseys/HoopsJersey.tsx
+++ src/components/jerseys/HoopsJersey.tsx
@@ -3,7 +3,7 @@ import { JerseyProps } from '.';
 
 export function HoopsJersey({ primaryColor = '#cccccc', secondaryColor = '#aaaaaa', className }: JerseyProps) {
     const id = `hoops-${primaryColor}-${secondaryColor}`.replace(/#/g, '');
-    const shirtPath = "M90.3,4.5C64.6,4.5,41.2,16.2,20.8,35.4c-13.6,12.8-20.9,29.9-20.9,47.3 c0,29.5,15.6,56.3,42,72.2c16.1,9.7,35,14.6,54.4,14.6c19.4,0,38.3-4.9,54.4-14.6c26.4-15.9,42-42.7,42-72.2 c0-17.4-7.3-34.5-20.9-47.3C139.4,16.2,116.1,4.5,90.3,4.5z";
+    const shirtPath = "M90,4.5c-29.3,0-53.1,23.8-53.1,53.1v104.4h106.2V57.6C143.1,28.3,119.3,4.5,90,4.5z M47.5,16.8 l-13.8,11.5c-2.3,1.9-3.7,4.8-3.7,7.7v20.4h120V36.4c0-2.9-1.4-5.8-3.7-7.7L132.5,16.8c-8.6-7.2-19.4-11.3-30.8-11.3h-21.4 C100.9,5.5,90.1,9.6,81.5,16.8L47.5,16.8z";
     
     return (
         <svg viewBox="0 0 180 180" className={className} xmlSpace="preserve">
@@ -19,6 +19,7 @@ export function HoopsJersey({ primaryColor = '#cccccc', secondaryColor = '#aaaaa
             <g clipPath={`url(#${id})`}>
                 <rect x="0" y="0" width="180" height="180" fill={`url(#pattern-${id})`} />
             </g>
+            <path d="M79,8.5c0,0-1.8,7.5,11,7.5s11-7.5,11-7.5" fill="none" stroke={primaryColor} strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" />
         </svg>
     );
 }
