664c291a1552db60c165ad74d1ef0b08acf239c9
Santiago Lopez <Santiago.lopez.toma@gmail.com>
2025-10-22 20:33:02 +0000

    La camsieta un se ve mal, mejoro sustancialmente pero sigue viendose mal

--- src/components/jerseys/StripesJersey.tsx
+++ src/components/jerseys/StripesJersey.tsx
@@ -2,7 +2,7 @@
 'use client';
 import { JerseyProps } from '.';
 
-const shirtPath = "M90,4.5c-29.3,0-53.1,23.8-53.1,53.1v104.4h106.2V57.6C143.1,28.3,119.3,4.5,90,4.5z M47.5,16.8 l-13.8,11.5c-2.3,1.9-3.7,4.8-3.7,7.7v20.4h120V36.4c0-2.9-1.4-5.8-3.7-7.7L132.5,16.8c-8.6-7.2-19.4-11.3-30.8-11.3h-21.4 C100.9,5.5,90.1,9.6,81.5,16.8L47.5,16.8z";
+const shirtPath = "M90 4.5C60.7 4.5 36.9 28.3 36.9 57.6v104.4h106.2V57.6C143.1 28.3 119.3 4.5 90 4.5zM47.5 16.8L33.7 28.3c-2.3 1.9-3.7 4.8-3.7 7.7v20.4h120V36.4c0-2.9-1.4-5.8-3.7-7.7L132.5 16.8c-8.6-7.2-19.4-11.3-30.8-11.3h-21.4C100.9 5.5 90.1 9.6 81.5 16.8L47.5 16.8z";
 
 export function StripesJersey({ primaryColor = '#cccccc', secondaryColor = '#aaaaaa', className }: JerseyProps) {
   const id = `stripes-jersey-${Math.random().toString(36).substr(2, 9)}`;
@@ -26,7 +26,7 @@ export function StripesJersey({ primaryColor = '#cccccc', secondaryColor = '#aaa
       <g clipPath={`url(#${id})`}>
         <rect x="0" y="0" width="180" height="180" fill={`url(#pattern-${id})`} />
       </g>
-       <path d="M79,8.5c0,0-1.8,7.5,11,7.5s11-7.5,11-7.5" fill="none" stroke="#000000" opacity="0.1" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" />
+       <path d="M79 8.5c0 0-1.8 7.5 11 7.5s11-7.5 11-7.5" fill="none" stroke={primaryColor} strokeWidth="6" strokeLinecap="round" strokeLinejoin="round" />
     </svg>
   );
 }
