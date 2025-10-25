2d0703030e4cceec4bb1402c92ab673b56226aa7
Santiago Lopez <Santiago.lopez.toma@gmail.com>
2025-10-25 18:26:06 +0000

    saca toda referencia a fake.ads que lo borré y esta generando problemas

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -5,7 +5,6 @@ config({ path: './.env' }); // Make sure to specify the path to your .env file
 import '@/ai/flows/suggest-player-improvements.ts';
 import '@/ai/flows/generate-balanced-teams.ts';
 import '@/ai/flows/get-match-day-forecast.ts';
-import '@/ai/flows/generate-fake-ad.ts';
 import '@/ai/flows/find-best-fit-player.ts';
 import '@/ai/flows/generate-player-card-image.ts';
 import '@/ai/flows/generate-group-summary.ts';
