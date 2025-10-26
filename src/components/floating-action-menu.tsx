e2977d2a77698a4baffd440071a14c94ef1ba77a
Santiago Lopez <Santiago.lopez.toma@gmail.com>
2025-10-26 13:38:22 +0000

    Vamos a sacar de los botones ftlotantes al Grupos

--- src/components/floating-action-menu.tsx
+++ src/components/floating-action-menu.tsx
@@ -1,7 +1,5 @@
-
 'use client';
 
-import { GroupsFab } from './groups/groups-fab';
 import { HelpChatDialog } from './help-chat-dialog';
 
 export function FloatingActionMenu() {
@@ -9,7 +7,6 @@ export function FloatingActionMenu() {
   return (
     <div className="fixed bottom-20 right-6 z-40 md:bottom-6 flex flex-col items-end gap-4">
         <HelpChatDialog />
-        <GroupsFab />
     </div>
   );
 }
