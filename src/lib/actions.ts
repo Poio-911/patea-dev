c59447903aded237e6cae49122bee89f13cf32ad
Santiago Lopez <Santiago.lopez.toma@gmail.com>
2025-10-27 19:19:04 +0000

    Failed to compile
    Next.js (14.2.4) is outdated (learn more)
    ./src/lib/a

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,7 +1,6 @@
 
 'use server';
 
-import 'dotenv/config';
 import { generateBalancedTeams, GenerateBalancedTeamsInput } from '@/ai/flows/generate-balanced-teams';
 import { suggestPlayerImprovements, SuggestPlayerImprovementsInput } from '@/ai/flows/suggest-player-improvements';
 import { getMatchDayForecast, GetMatchDayForecastInput } from '@/ai/flows/get-match-day-forecast';
