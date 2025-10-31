ffbab058f52ca1b3cef75faf5b99c2a487ec81ef
Santiago Lopez <Santiago.lopez.toma@gmail.com>
2025-10-31 16:08:04 +0000

    Try fixing this error: `Build Error: Failed to compile. Error source: ./

--- src/components/client-providers.tsx
+++ src/components/client-providers.tsx
@@ -6,7 +6,7 @@ import { initializeFirebase } from '@/firebase/index';
 import type { FirebaseApp } from 'firebase/app';
 import type { Auth } from 'firebase/auth';
 import type { Firestore } from 'firebase/firestore';
-import { UserProvider } from './auth/use-user';
+import { UserProvider } from '@/firebase/auth/use-user';
 import { MainNav } from '@/components/main-nav';
 import { ThemeProvider } from 'next-themes';
 import { useJsApiLoader } from '@react-google-maps/api';
