faf9fb235b27beeaf7abe78eac0f027aa1255ed9
Santiago Lopez <Santiago.lopez.toma@gmail.com>
2025-11-05 20:17:32 +0000

    main-nav.tsx:276 Warning: React.jsx: type is invalid -- expected a strin

---
+++ src/components/icons/index.ts
@@ -0,0 +1,17 @@
+'use client';
+
+export * from './analysis-icon';
+export * from './coach-icon';
+export * from './evaluation-icon';
+export * from './find-match-icon';
+export * from './goal-icon';
+export * from './mate-icon';
+export * from './match-icon';
+export * from './player-marker-icon';
+export * from './positions';
+export * from './shirt-icon';
+export * from './soccer-player-icon';
+export * from './tactics-icon';
+export * from './teams-icon';
+export * from './vest-icon';
+export * from './whatsapp-icon';
