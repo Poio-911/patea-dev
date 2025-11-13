49134f0df7f63b0f9250b0597a5767eebe5fd35c
Santiago Lopez <Santiago.lopez.toma@gmail.com>
2025-11-13 20:22:09 +0000

    Try fixing this error: `Unhandled Runtime Error: ReferenceError: Control

--- src/components/competitions/create-league-dialog.tsx
+++ src/components/competitions/create-league-dialog.tsx
@@ -1,7 +1,8 @@
+
 'use client';
 
 import { useState } from 'react';
-import { useForm } from 'react-hook-form';
+import { useForm, Controller } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
